ef86eadb9d6ccf0ddcb6ea57d2fe7e9eace6ab9c
Dasha Kurilova <kurilovadk@gmail.com>
2020-08-21 20:52:46 +0300

    Delete main.controller

---
+++ iLetter/iLetter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iLetter
+//
+//  Created by Дарья on 21/08/2020.
+//  Copyright © 2020 Дарья. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
